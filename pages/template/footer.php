a9cdf7b1cee4cd596ff0512733aa9c96f50b553e
Pedro Campos <phenriquebcampos@gmail.com>
2023-05-16 13:43:02 -0300

    new coisas

---
+++ pages/template/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <p>Get Connected:</p>
+    <div>
+        <ul class="wrapper">
+            <li class="icon github">
+                <span><i class="fab fa-github"></i></span>
+            </li>
+            <li class="icon linkedin">
+                <span><i class="fab fa-linkedin"></i></span>
+            </li>
+            <li class="icon twitter">
+                <span><i class="fab fa-twitter"></i></span>
+            </li>
+        </ul>
+    </div>
+    <p>&copy; 2023 Minha Página. Todos os direitos reservados.</p>
+</footer>
