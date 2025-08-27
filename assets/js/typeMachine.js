f1d36d9942d47000e22602c708be373b64b17f73
Pedro Campos <phenriquebcampos@gmail.com>
2025-08-27 15:46:45 -0300

    portifolio

--- assets/js/typeMachine.js
+++
@@ -1,9 +0,0 @@
-const typed = new Typed('#Typed', {
-    strings: ['Developer', 'Pixel Artist', 'Ilustrator', 'GameDev', 'WebDev', 'unemployed'],
-    backSpeed: 100,
-    backDelay: 1750,
-    typeSpeed: 125,
-    loop: true,
-    cursorChar: '▌',
-    autoInsertCss: true,
-});
